15afba565580efed3c6429ae3266f9976db00cb9
brandongatlin <brandongatlin1981@me.com>
2018-05-20 00:09:14 -0500

    small fixes

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -3,18 +3,8 @@ $( document ).ready( function () {
 
   $.get( "/dashboard/comments", function ( res ) {
     console.log( "got /dashboard front end" );
-
-    // if ( res ) {
-    //   console.log( "front comments res is:", res );
-    //
-    //   // for ( var i = 0; i < res.length; i++ ) {}
-    //
-    // }
   } )
 
-  // $.get( "/comments", function ( res ) {
-  //   console.log( "hit new comment route on front end" );
-  // } )
 
   $( "#submit-donation" ).on( "click", function ( event ) {
     event.preventDefault();
@@ -23,19 +13,17 @@ $( document ).ready( function () {
     var food_description = $( "#food-description-input" ).val().trim();
     var food_quantity = $( "#food-quantity-input" ).val().trim()
 
-
     var newDonation = {
       food_type: food_type,
       food_description: food_description,
       food_quantity: food_quantity
     }
 
-    console.log( "front end newDonation is:", newDonation );
-
+    $( "#food-item-input" ).val( '' );
+    $( "#food-description-input" ).val( '' );
+    $( "#food-quantity-input" ).val( '' );
 
     $.post( "/members/newdonation", newDonation, function ( req, res ) {
-
-      console.log( "/newdonation hit on front end" );
 
     } )
   } )
