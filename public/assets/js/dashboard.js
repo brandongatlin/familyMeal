ddef0f6a4dac864b824e407b863a6db9c18a8bd7
brandongatlin <brandongatlin1981@me.com>
2018-05-24 08:30:17 -0500

    added user gps location to donation map

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,6 +1,8 @@
 var markers;
 var content;
 
+var map, infoWindow;
+
 function initMap( moredata ) {
   // console.log( "more data is:", moredata )
 
@@ -34,6 +36,7 @@ function initMap( moredata ) {
             var infowindow = new google.maps.InfoWindow( {
               content: `<h6><a href=https://www.google.com/search?q=${name} target="_blank">${name}</a></h6>`
 
+
             } );
             google.maps.event.addListener( marker, 'click', function () {
               infowindow.open( map, marker );
@@ -41,8 +44,49 @@ function initMap( moredata ) {
           }
         }
       } )( name ) );
+
+    infoWindow = new google.maps.InfoWindow;
+
+
+    if ( navigator.geolocation ) {
+      navigator.geolocation.getCurrentPosition( function ( position ) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition( pos );
+        infoWindow.setContent( 'You are here' );
+        infoWindow.open( map );
+        map.setCenter( pos );
+      }, function () {
+        handleLocationError( true, infoWindow, map.getCenter() );
+      } );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError( false, infoWindow, map.getCenter() );
+    }
   }
-}
+
+
+  function handleLocationError( browserHasGeolocation, infoWindow, pos ) {
+    infoWindow.setPosition( pos );
+    infoWindow.setContent( browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.' );
+    infoWindow.open( map );
+
+
+
+
+
+  }
+
+
+
+
+
+} //end initMap
 
 
 $( document ).ready( function () {
